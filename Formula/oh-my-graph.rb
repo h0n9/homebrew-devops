da5443ee51a8a2a1b7623099467e70db0581ea94
Jaehong Ahn <monsieur.ahn@gmail.com>
2026-06-23 20:24:58 +0900

    Add OhMyGraph formula for MCP-compatible knowledge graph server

---
+++ Formula/oh-my-graph.rb
@@ -0,0 +1,38 @@
+class OhMyGraph < Formula
+  desc "MCP-compatible knowledge graph server with HTTP transport and in-memory caching"
+  homepage "https://github.com/h0n9/oh-my-graph"
+  license "Apache-2.0"
+  version "0.1.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/h0n9/oh-my-graph/releases/download/v#{version}/oh-my-graph_darwin_arm64.tar.gz"
+      sha256 "PLACEHOLDER" # darwin_arm64
+    end
+    on_intel do
+      url "https://github.com/h0n9/oh-my-graph/releases/download/v#{version}/oh-my-graph_darwin_amd64.tar.gz"
+      sha256 "PLACEHOLDER" # darwin_amd64
+    end
+  end
+
+  def install
+    bin.install "oh-my-graph"
+  end
+
+  service do
+    run [opt_bin/"oh-my-graph", "--port", "7780"]
+    keep_alive true
+    log_path var/"log/oh-my-graph.log"
+    error_log_path var/"log/oh-my-graph.log"
+  end
+
+  test do
+    port = free_port
+    pid = fork { exec bin/"oh-my-graph", "--port", port.to_s, "--data", testpath.to_s }
+    sleep 1
+    assert_match "oh-my-graph", shell_output("curl -sf http://localhost:#{port}/")
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
